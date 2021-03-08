365adac7d4b634f122593676b9b6c7e61ec512f9
Innokentiy945 <51906417+Innokentiy945@users.noreply.github.com>
2021-03-08 14:06:34 +0300

    fix

--- TaskOne/Counting.py
+++ TaskOne/Counting.py
@@ -1,20 +1,31 @@
+class Division:
+    def checkDivision(number, divider):
+        if number % divider == 0:
+            return True
+        else:
+            return False
+
+
 class Initial:
-    def __int__(self, number):
-        self.number = number
+    def __int__(self, count, dividerOne, divederTwo):
+        self.count = count
+        self.dividerOne = dividerOne
+        self.dividerTwo = divederTwo
 
 
-class Counting(Initial):
+class Counting(Initial, Division):
     init = Initial()
-    init.number = 100
-    for init.number in range(init.number, 0, -1):
+    div = Division
+    init.count = 100
+    init.dividerOne = 5
+    init.dividerTwo = 3
+    for init.count in range(init.count, 0, -1):
 
-        if init.number % 15 == 0:
+        if div.checkDivision(init.count, init.dividerOne*init.dividerTwo):
             print("Agile", "Software", "Testing")
-        elif init.number % 5 == 0:
+        elif div.checkDivision(init.count, init.dividerOne):
             print("Agile")
-        elif init.number % 3 == 0:
+        elif div.checkDivision(init.count, init.dividerTwo):
             print("Software")
         else:
-            print(init.number)
-
-
+            print(init.count)
